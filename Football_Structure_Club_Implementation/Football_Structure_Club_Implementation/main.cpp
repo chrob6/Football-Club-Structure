ccb290de852dcb0eebfd11f42414f59e343042de
chrob6 <chrobak.mar6@gmail.com>
2020-10-23 11:54:24 +0200

    Start implementation

---
+++ Football_Structure_Club_Implementation/Football_Structure_Club_Implementation/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Tu bedzie doskonaly system" << endl;
+	cin.get();
+
+
+	return 0;
+}
+
